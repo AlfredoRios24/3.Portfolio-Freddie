cfe7c094ce21b7cdb7767fecf6497e17d47e0a2d
AlfredoRios24 <alfredoerios_03@hotmail.com>
2024-12-02 14:53:30 +0100

    main

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -33,20 +33,25 @@ const Footer = ({ language }) => {
         <footer className="bg-gray-900 text-white">
             <div className="flex flex-col items-center p-10">
                 <h2 className="text-2xl font-bold mb-4">{TRANSLATION_FOOTER[language].titleContact}</h2>
-                <ul className="flex space-x-6 justify-center mb-4">
-                    <li className="text-xl">{PROFILE_INFO[0].Phone}</li>
-                    <li>
-                        <button onClick={copyPhone} className="text-xl hover:text-cyan-300">
+                
+                {/* Información en columna con margen adecuado */}
+                <ul className="flex flex-col mb-4 space-y-4 w-96 items-center">
+                    <li className="flex items-center text-xl">
+                        <span>{PROFILE_INFO[0].Phone}</span>
+                        <button onClick={copyPhone} className="ml-4 text-xl hover:text-cyan-300">
                             <ion-icon name="copy-outline"></ion-icon>
                         </button>
                     </li>
-                    <li className="text-xl">{PROFILE_INFO[0].Email}</li>
-                    <li>
-                        <button onClick={copyEmail} className="text-xl hover:text-cyan-300">
+
+                    <li className="flex items-center text-xl">
+                        <span>{PROFILE_INFO[0].Email}</span>
+                        <button onClick={copyEmail} className="ml-4 text-xl hover:text-cyan-300">
                             <ion-icon name="copy-outline"></ion-icon>
                         </button>
                     </li>
                 </ul>
+                
+                {/* Botón para volver arriba */}
                 <button
                     onClick={handleScrollToTop}
                     className="text-3xl bg-cyan-300 text-white rounded-full shadow-lg p-3 hover:bg-cyan-500 fixed bottom-10 right-10"
