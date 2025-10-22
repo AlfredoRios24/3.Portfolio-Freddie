4cf28861d6423916d564ef4faef3b676f88be07c
AlfredoRios24 <alfredoerios_03@hotmail.com>
2025-10-22 12:20:53 +0200

    changes gramatical language

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -31,8 +31,8 @@ export const PROJECT_INFO = [
             en: "Register Note"
         },
         Description: {
-            es: "Proyecto Fullstack: Desarrollo de una herramienta de trabajo para crear, editar y guardar notas. Se han usado diferentes técnologias, des del lado del Fronted: ReactJS y TypeScript y del Backend: Spring y Mysql. Esta experiéncia me permitio profundizar en funcionalidades como hacer las llamadas al servidor, comprender parte de un funcionamiento api curl y crear mis primeras pruebas unitarias e integrales.",
-            en: "Project Fullstack: Development of a working tool to create, edit and save notes. Different technologies have been used, from the frontend side: ReactJS and TypeScript and from the backend side: Spring and Mysql. This experience allowed me to deepen in functionalities such as making calls to the server, understanding part of a curl api operation, unit and integral priebas."
+            es: "Proyecto Fullstack: Desarrollo de una herramienta de trabajo para crear, editar y guardar notas. Se han usado diferentes tecnologías, des del lado del Fronted: ReactJS y TypeScript y del Backend: Spring y Mysql. Esta experiencia me permitió profundizar en funcionalidades como hacer las llamadas al servidor, comprender parte de un funcionamiento API CRUD y crear mis primeras pruebas unitarias e integrales.",
+            en: "Project Fullstack: Development of a working tool to create, edit and save notes. Different technologies have been used, from the frontend side: ReactJS and TypeScript and from the backend side: Spring and Mysql. This experience allowed me to deepen in functionalities such as making calls to the server, understanding part of a CRUD API operation, unit and integral priebas."
         },
         Image: fondonotes,
 
@@ -61,7 +61,7 @@ export const PROJECT_INFO = [
             en: "Katas of Test-Driven Development."
         },
         Description: {
-            es: "Este proyecto contiene una colección de Katas de programación implementadas en Java utilizando TDD (Test-Driven Development) como práctica principal. El objetivo es reforzar los conceptos de desarrollo guiado por pruebas, adquirir bases sólidas en testing y mejorar como desarrollador mediante ejercicios iterativos y enfocados en la calidad del código.",
+            es: "Este proyecto contiene una colección de Katas de programación implementadas en Java utilizando TDD (Test-Driven Development) como práctica principal. El objetivo es reforzar los conceptos de desarrollo guiado por pruebas, adquirir bases sólidas en TESTING y mejorar como desarrollador mediante ejercicios iterativos y enfocados en la calidad del código.",
             en: "This project contains a collection of programming Katas implemented in Java using TDD (Test-Driven Development) as the main practice. The aim is to reinforce the concepts of test-driven development, acquire a solid foundation in testing, and improve as a developer through iterative exercises focused on code quality."
         },
         Image: codekata,
@@ -80,7 +80,7 @@ export const PROJECT_INFO = [
             en: "Web Serralleria Fanals"
         },
         Description: {
-            es: "Desarrollo de una pagina web con el diseño inspirado en Elementor de Wordpress, la construcción de la web se ha desarrolado con ReactTS y TailwindCSS, el despliegue se ha realizado con Vercel y codigo en GitHub. Se estima que pueda ser pública en un futuro cercano. Esta experiencia me permitio adquirir conocimientos básicos de diseño de webs.",
+            es: "Desarrollo de una página web con el diseño inspirado en Elementor de Wordpress, la construcción de la web se ha desarrollado con ReactTS y TailwindCSS, el despliegue se ha realizado con Vercel y código en GitHub. Se estima que pueda ser pública en un futuro cercano. Esta experiencia me permitió adquirir conocimientos básicos de diseño de webs.",
             en: "Development of a website with a design inspired by Wordpress' Elementor. The website was built using ReactTS and TailwindCSS, deployed with Vercel, and coded in GitHub. It is expected to be made public in the near future. This experience allowed me to acquire basic knowledge of web design."
         },
         Image: fondowbfanals,
